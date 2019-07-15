59c30fdcec447c9eb923294348e233283a3927d8
JWeber2113 <weberjoseph9393@yahoo.com>
2019-07-15 15:58:16 -0700

    basic validator added

--- apps/tvShows_app/models.py
+++ apps/tvShows_app/models.py
@@ -1,4 +1,16 @@
+from __future__ import unicode_literals
 from django.db import models
+
+class ShowMananger(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 2:
+            errors["title"] = "Title should be at least 2 characters"
+        if len(postData['network']) < 3:
+            errors["network"] = "Network should be at least 3 characters"
+        if len(postData['desc']) < 10:
+            errors["desc"] = "Description should be at least 5 characters"
+        return errors
 
 class Show(models.Model):
     title = models.CharField(max_length = 255)
@@ -7,3 +19,4 @@ class Show(models.Model):
     description = models.CharField(max_length = 255)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+    objects = ShowMananger()
--- apps/tvShows_app/views.py
+++ apps/tvShows_app/views.py
@@ -1,40 +1,46 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import Show
 
 def index(request):
-    print('*'*100)
-    print('showing all tv shows')
-
+    
     context = {
         'shows' : Show.objects.all()
     }
     return render(request, 'tvShows_app/index.html', context)
 
-def new(request):
-    print('*'*100)
-    print('add a new show!!')
 
+def new(request):
+    
     return render(request, 'tvShows_app/new_show.html')
 
+
 def create(request):
-    print('*'*100)
-    print('in the create method!!')
 
-    new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], releaseDate=request.POST['release'], description=request.POST['desc'])
+    errors = Show.objects.basic_validator(request.POST)
 
-    return redirect(f'/shows/{new_show.id}')
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect('/shows/new')
+    else:
+    
+        new_show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], releaseDate=request.POST['release'], description=request.POST['desc'])
+
+        return redirect(f'/shows/{new_show.id}')
+
 
 def about(request, show_id):
-    print('*'*100)
-    print('in the about page!!')
+    
     context = {
         'show' : Show.objects.get(id = show_id)
     }
     return render(request, 'tvShows_app/about.html', context)
 
+
 def edit(request, show_id):
-    print('*'*100)
-    print('in the edit page!!')
+    
     context = {
         'show' : Show.objects.get(id = show_id)
     }
@@ -42,14 +48,22 @@ def edit(request, show_id):
 
 
 def update(request, show_id):
-    show = Show.objects.get(id = show_id)
-    show.title = request.POST['updateTitle']
-    show.network = request.POST['updateNetwork']
-    show.releaseDate = request.POST['updateRelease']
-    show.description = request.POST['updateDesc']
-    show.save()
+    errors = Show.objects.basic_validator(request.POST)
 
-    return redirect(f'/shows/{show.id}')
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        show = Show.objects.get(id = show_id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.releaseDate = request.POST['release']
+        show.description = request.POST['desc']
+        show.save()
+
+        return redirect(f'/shows/{show.id}')
 
 
 def destroy(request, show_id):
